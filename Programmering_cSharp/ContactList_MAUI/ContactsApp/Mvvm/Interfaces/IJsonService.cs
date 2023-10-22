68c6cd6aa3b1b6553e7dce37771ee90c8cea38c8
jaghetermarkus <143387643+jaghetermarkus@users.noreply.github.com>
2023-10-22 22:17:16 +0200

    Removed Contacts_MAUI folder

--- Programmering_cSharp/ContactList_MAUI/ContactsApp/Mvvm/Interfaces/IJsonService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ContactsApp.Interfaces
-{
-    public interface IJsonService
-    {
-        bool SaveToJson(string contentAsJson, string filePath);
-        string ReadFromJson(string filePath);
-    }
-}
